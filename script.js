aa867bf02497f9ef0a1d17ab6a25a8f98d5242fa
Nayeem-condify <webdevlopment2306@gmail.com>
2024-01-20 18:35:27 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+console.log (document)
+
+document.getElementById("one").innerHTML = "Java"
+
+document.write ("javaScript")
+
+window.alert ("JavaScript Class 1")
+
+console.log ("JavaScript")
+
+let firstname = "Java"
+
+let lastname = "Scriptjs"
+
+var a = 5;
+let b = 10;
+const c = 15;
+
+var a = 6;
+
+b = 11
+
+console.log (a)
+console.log (b)
+console.log (c)
+
+console.log(a + b)
+console.log(a + b + c)
+console.log(a + b - c)
+console.log( a * b )
+console.log( a / b )
+console.log( a > b )
+console.log( a < b )
+
+
+
